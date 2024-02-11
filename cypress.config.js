e4b4d621fedf5bb9e8f257ef1d17199d9006dd3e
Sumant Kumar <sumant.nitmz94@gmail.com>
2024-02-12 00:42:19 +0100

    added project id in config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "6b30df12-0cd7-43e5-8e7b-531e27715c4d",
   video:true,
   e2e: {
     setupNodeEvents(on, config) {
