97b781aaeb58e575254928720216664902b0f6fd
Sumant Kumar <sumant.nitmz94@gmail.com>
2024-02-12 12:42:43 +0100

    updated .yml

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "6b30df12-0cd7-43e5-8e7b-531e27715c4d",
+  projectId: "o4hm43",
   video:true,
   e2e: {
     setupNodeEvents(on, config) {
